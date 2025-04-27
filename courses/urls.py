533c2c815c4f1ba1d7f4e18897b4e0fdeb7be787
Mohammad Jibran <yunisquadri99@gmail.com>
2025-04-28 00:42:09 +0530

    enollment updated

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import CourseListCreateView, CourseDetailView, EnrollCourseView
+from .views import CourseListCreateView, CourseDetailView, EnrollCourseView , MyEnrollmentsView, EnrollmentViewSet
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register(r'enrollments', EnrollmentViewSet, basename='enrollment')
+
 
 urlpatterns = [
     path('', CourseListCreateView.as_view(), name='course-list-create'),              # No 'courses/' prefix here
     path('<int:id>/', CourseDetailView.as_view(), name='course-detail'),
     path('enroll/', EnrollCourseView.as_view(), name='course-enroll'),
+    path('my-enrollments/', MyEnrollmentsView.as_view(), name='my-enrollments'),
+    path('check-enrollment/', MyEnrollmentsView.as_view(), name='check-enrollment'),
+    router.urls,
 ]
