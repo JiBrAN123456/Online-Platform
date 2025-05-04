4e5aad71de3826d353a9e6121b34a53435d95477
Mohammad Jibran <yunisquadri99@gmail.com>
2025-05-05 00:21:30 +0530

    payments started

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import CourseReview , Course , Lesson , LessonComment , LessonLike , Bookmark , Notification
 # Register your models here.
+
+admin.site.register(CourseReview)
+
+admin.site.register(Course)
+
+admin.site.register(Lesson)
+
+admin.site.register(LessonLike)
+
+admin.site.register(LessonComment)
+
+admin.site.register(Bookmark)
+
+admin.site.register(Notification)
