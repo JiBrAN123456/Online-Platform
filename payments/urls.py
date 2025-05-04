e3e61718096585b9b613456928c282dfbe2b9350
Mohammad Jibran <yunisquadri99@gmail.com>
2025-05-05 00:16:25 +0530

    payments started

--- payments/urls.py
+++ payments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreatePaymentView
+from .views import CreatePaymentView , CreatePaymentIntentView
 
 urlpatterns = [
     path('create/', CreatePaymentView.as_view(), name='create-payment'),
+    path('create-intent/', CreatePaymentIntentView.as_view(), name='create-payment-intent'),
 ]
