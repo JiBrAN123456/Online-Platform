fc82ed393d3dae00768404847ad8532baf3da80b
Mohammad Jibran <yunisquadri99@gmail.com>
2025-05-10 02:08:49 +0530

    real time notifications started

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+# notifications/models.py
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notifications_from_ws'  # changed this line
+    )
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"To: {self.user.username} - {self.message[:30]}"
